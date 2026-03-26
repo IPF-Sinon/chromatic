524ad24de8488de403683a82afaa2583038547c5
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2026-03-26 18:13:28 +0800

    chore: update breeze-js version

--- deps/breeze-js.lua
+++ deps/breeze-js.lua
@@ -1,28 +1,42 @@
+local BREEZE_JS_VERSION = "2026.03.26"
+local BREEZE_JS_HASH = "cf24ff9f0fc88a526eb9eed8e379d28a5f6a4566"
+local BREEZE_JS_SOURCEDIR = path.join(os.scriptdir(), "../", "../GitHub/breeze-js")
+local USE_LOCAL = os.exists(BREEZE_JS_SOURCEDIR)
 
-local BREEZE_JS_VERSION = "2026.03.11"
-local BREEZE_JS_HASH = "a8252754b59333463defaa316f4a9d567237c893"
+if USE_LOCAL then
+    print("Using local breeze-js source directory: " .. BREEZE_JS_SOURCEDIR)
+end
 
 package("breeze-quickjs-ng")
     set_description("The breeze-quickjs-ng package")
-    set_urls("https://github.com/breeze-shell/breeze-js.git")
-    add_versions(BREEZE_JS_VERSION, BREEZE_JS_HASH)
-    add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    if USE_LOCAL then
+        set_sourcedir(BREEZE_JS_SOURCEDIR)
+    else
+        set_urls("https://github.com/breeze-shell/breeze-js.git")
+        add_versions(BREEZE_JS_VERSION, BREEZE_JS_HASH)
+    end
+    add_configs("shared", { description = "Build shared library.", default = false, type = "boolean", readonly = true })
 
-    on_install(function (package)
+    on_install(function(package)
         import("package.tools.xmake").install(package)
     end)
 
 
 package("breeze-js-runtime")
     set_description("The breeze-js-runtime package")
-    set_urls("https://github.com/breeze-shell/breeze-js.git")
-    add_versions(BREEZE_JS_VERSION, BREEZE_JS_HASH)
-    add_deps("breeze-quickjs-ng", {public=true})
-    add_deps("yalantinglibs 0c98464dd202aaa6275a8da3297719a436b8a51a", {configs={ssl=true}})
+    if USE_LOCAL then
+        set_sourcedir(BREEZE_JS_SOURCEDIR)
+    else
+        set_urls("https://github.com/breeze-shell/breeze-js.git")
+        add_versions(BREEZE_JS_VERSION, BREEZE_JS_HASH)
+    end
+    add_deps("breeze-quickjs-ng", { public = true })
+    add_deps("yalantinglibs 0c98464dd202aaa6275a8da3297719a436b8a51a", { configs = { ssl = true } })
     add_deps("ctre")
+    add_deps("concurrentqueue", { public = true })
 
-    add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    add_configs("shared", { description = "Build shared library.", default = false, type = "boolean", readonly = true })
 
-    on_install(function (package)
+    on_install(function(package)
         import("package.tools.xmake").install(package)
     end)
